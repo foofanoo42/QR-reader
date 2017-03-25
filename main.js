5b2482e89b8adbe99152b477967dafe9d775522e
Foofanoo <sr9383@gmail.com>
2017-03-25 23:22:07 +1300

    corrected the jquery code

--- main.js
+++ main.js
@@ -9,11 +9,43 @@ $(document).ready(function(){
 		}
 		
 		
-		$('#reader').videoElement.requestFullscreen();
+		
 	);
+	$('#reader').videoElement.requestFullscreen();
+	
+		
+		
+		
+
 });
 
 //if(navigator.userAgent.match(/Android/i)){
   //  window.scrollTo(0,1);
  //}
+/*
 
+	
+		
+		var x = document.getElementById("demo");
+		function getLocation() {
+			if (navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(showPosition);
+			} else {
+				x.innerHTML = "Geolocation is not supported by this browser.";
+			}
+		}
+		function showPosition(position) {
+			x.innerHTML = "Latitude: " + position.coords.latitude + 
+			"<br>Longitude: " + position.coords.longitude; 
+		}
+		
+		
+		document.getElementById("demo").addEventListener('mousedown', function (event) {
+			getLocation();
+		});
+		
+		
+	
+	*/
+	
+	
